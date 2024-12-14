083abdbd55ea2dc587ed67f492cdcb05884bbba5
Jonathan Wohl <jon@jonwohl.com>
2024-12-14 13:05:10 -0600

    Default settings?

--- stage2-zf/00-kiosk/files/firstrun.sh
+++ stage2-zf/00-kiosk/files/firstrun.sh
@@ -4,8 +4,14 @@ echo ">> FIRST RUN"
 
 echo
 echo ">> Enabling Read-Only Overlay File System"
-sudo raspi-config nonint enable_overlayfs
-sudo raspi-config nonint enable_bootro
+# sudo raspi-config nonint enable_overlayfs
+# sudo raspi-config nonint enable_bootro
+sudo raspi-config nonint do_change_locale en_US.UTF-8
+sudo raspi-config nonint do_boot_splash 1
+sudo raspi-config nonint do_boot_behaviour B2
+sudo raspi-config nonint do_blanking 1
+sudo raspi-config nonint memory_split 512
+sudo raspi-config nonint do_serial 1
 
 echo
 echo ">> Removing First Run Script"
