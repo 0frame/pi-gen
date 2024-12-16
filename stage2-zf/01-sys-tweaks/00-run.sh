6e2b595315195c7fe3421e9def5f24610649d5aa
Jonathan Wohl <jon@jonwohl.com>
2024-12-15 22:38:08 -0600

    disable ssh, make run file executable

--- stage2-zf/01-sys-tweaks/00-run.sh
+++ stage2-zf/01-sys-tweaks/00-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-
+echo ">> Configuring Autologin"
 install -m 644 files/etc/systemd/system/getty@tty1.service.d/autologin.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf"
